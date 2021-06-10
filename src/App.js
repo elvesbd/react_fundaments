f42f68e0728d2ecabc92ed1abe1887390bc83a52
elves brito <elvesbd@yahoo.com>
2021-06-10 15:16:49 -0300

    React_fundaments: aplying styled components

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { useState, useMemo, createContext } from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import GlobalStyle from './styles/global';
+import Layout from './components/Layout';
+
+import themes from './styles/themes';
+
+export const ThemeContext = createContext('dark');
+
+
+function App() {
+  const [theme, setTheme] = useState('dark');
+
+  const currentTheme = useMemo(() => {
+    return themes[theme] || themes.dark
+  }, [theme]);
+
+  function handleToggleTheme() {
+    setTheme(prevState => prevState === 'dark' ? 'light' : 'dark');
+  };
+
+  return (
+    <ThemeContext.Provider 
+      value={{
+        onToggleTheme: handleToggleTheme, 
+        selectedTheme: theme
+      }}
+    >
+      <ThemeProvider theme={currentTheme}>
+        <GlobalStyle />
+        <Layout />
+      </ThemeProvider>
+    </ThemeContext.Provider>
+  );
+};
+
+export default App;
